922add52b81ab5b078fccb5048973a6c8ebc7d34
Ivailo663 <ivailo663@gmail.com>
2020-10-16 17:36:13 +0300

    Jotto App

--- src/Congrats.js
+++ src/Congrats.js
@@ -1,13 +1,23 @@
 import React from "react";
-
-export default (props) => {
-  return props.success ? (
-    <div data-test="component-congrats">
-      <span data-test="congrats-message">
-        Congratulations! You guessed the world!
-      </span>
-    </div>
-  ) : (
-    ""
-  );
+import PropTypes from "prop-types";
+const Congrats = (props) => {
+  //   let arr = new Promise(() => {});
+  //   console.log(arr.constructor === Promise);
+  if (props.success) {
+    return (
+      <div data-test="component-congrats">
+        <span data-test="congrats-message">
+          Congratulations! You guessed the world!
+        </span>
+      </div>
+    );
+  } else {
+    return <div data-test="component-congrats"></div>;
+  }
 };
+
+Congrats.propTypes = {
+  success: PropTypes.bool.isRequired,
+};
+
+export default Congrats;
